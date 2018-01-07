a14617003c65e44cdf0639764065a4fa64fd8a24
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-01-07 20:51:44 +0000

    Complete Day 6

---
+++ 2017/day6/day6.py
@@ -0,0 +1,62 @@
+import numpy as np
+import sys
+
+PART1_SOLUTION = 4074
+PART2_SOLUTION = 2793
+
+
+def load_input_data():
+    with open("input") as f:
+        return [int(data) for data in f.readline().strip().split("\t")]
+
+
+def part1():
+    memory_banks = np.array(load_input_data())
+    memory_banks_history = []
+    memory_banks_history.append(memory_banks.tolist())
+    redistribution_cycles = 0
+    while True:
+        selected_bank = np.argmax(memory_banks)
+        blocks = memory_banks[selected_bank]
+        memory_banks[selected_bank] = 0
+        for i in range(selected_bank, selected_bank + blocks):
+            memory_banks[(i + 1) % memory_banks.size] += 1
+        redistribution_cycles += 1
+        if memory_banks.tolist() in memory_banks_history:
+            break
+        memory_banks_history.append(memory_banks.tolist())
+    return redistribution_cycles
+
+
+def find_memory_banks(memory_banks, memory_banks_history):
+    for i, m in enumerate(memory_banks_history):
+        if m == memory_banks:
+            return i
+
+
+def part2():
+    memory_banks = np.array(load_input_data())
+    memory_banks_history = []
+    memory_banks_history.append(memory_banks.tolist())
+    redistribution_cycles = 0
+    while True:
+        selected_bank = np.argmax(memory_banks)
+        blocks = memory_banks[selected_bank]
+        memory_banks[selected_bank] = 0
+        for i in range(selected_bank, selected_bank + blocks):
+            memory_banks[(i + 1) % memory_banks.size] += 1
+        k = find_memory_banks(memory_banks.tolist(), memory_banks_history)
+        if k is not None:
+            break
+        memory_banks_history.append(memory_banks.tolist())
+        redistribution_cycles += 1
+    return redistribution_cycles - k + 1
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "test":
+        assert part1() == PART1_SOLUTION
+        assert part2() == PART2_SOLUTION
+    else:
+        print(part1())
+        print(part2())
